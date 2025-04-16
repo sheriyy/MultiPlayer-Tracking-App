80485f8b75d5badbc8484bbaf2279f9f272881a6
Shubham <82511219+sheriyy@users.noreply.github.com>
2025-04-16 02:56:46 +0200

    Update Button.js

--- gcp_vision_proto/src/Button/Button.js
+++ gcp_vision_proto/src/Button/Button.js
@@ -1,1 +1,16 @@
+import Loading from "../images/Loading.svg";
+import './Button.css'
 
+const Button = ({label, isLoading, handleClick}) => {
+     
+    return(
+        <button className="analysis_button">
+           { isLoading ?
+                <img src={Loading} alt="Loading" className="loadingIcon"/>
+            :
+                <p>{label}</p>}
+        </button>
+    )
+}
+
+export default Button
