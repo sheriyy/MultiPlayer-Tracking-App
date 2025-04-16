10e6356309c0c6e53928ae03cee009d369b95e5f
Shubham <82511219+sheriyy@users.noreply.github.com>
2025-04-16 03:03:21 +0200

    Update VideoAnalysis.js

--- gcp_vision_proto/src/VideoAnalysis/VideoAnalysis.js
+++ gcp_vision_proto/src/VideoAnalysis/VideoAnalysis.js
@@ -1,1 +1,31 @@
 
+import './VideoAnalysis.css'
+import VideoThumbnail from '../images/VideoThumbnail.svg'
+import VideoIntelligenceLogo from '../images/video-intelligence-logo.svg'
+import Button from '../Button/Button';
+
+const VideoAnalysis = () => {
+
+    const analyzeVideo =() =>{
+        fetch("/videoIntelligence").then( 
+          res => res.json()
+          ).then(
+            data => {
+              console.log(data);
+            //   setLogoData(data)
+            }
+          )
+      }
+
+    return (
+        <div className="video-analysis-container">
+            <img src={VideoThumbnail} />
+            <div className='video-button-layout'>
+                <Button label={"Analyze video for faces"} isLoading={false} handleClick= {analyzeVideo}/>
+                <img src={VideoIntelligenceLogo} className='video-intelligence-logo' alt="logo"/>
+            </div>
+        </div>
+    )
+}
+
+export default VideoAnalysis;
