380b364c877fb3b42d8589c39fadd57c1b1bb573
Shubham <82511219+sheriyy@users.noreply.github.com>
2025-04-16 03:01:27 +0200

    Update SponsorDetails.js

--- gcp_vision_proto/src/SponsorDetails/SponsorDetails.js
+++ gcp_vision_proto/src/SponsorDetails/SponsorDetails.js
@@ -1,1 +1,15 @@
+import './SponsorDetails.css'
+import PlaceholderIMG from '../images/placeholder.jpg'
 
+const SponsorDetails = ({driverImage, sponsorArray}) => {
+    return (
+        <div className='sponsor-details'>
+            <img src={PlaceholderIMG} className='driver-image'/>
+            <table className='sponsor-list'>
+                {sponsorArray.map(sponsorName => <tr><td className='sponsor-name'>{sponsorName}</td></tr>)}
+            </table>
+        </div>
+    );
+}
+
+export default SponsorDetails;
