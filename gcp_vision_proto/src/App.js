b6b43fc3b088284ca6fe07c710bbfdf550366730
Shubham <82511219+sheriyy@users.noreply.github.com>
2025-04-16 03:05:28 +0200

    Update App.js

--- gcp_vision_proto/src/App.js
+++ gcp_vision_proto/src/App.js
@@ -1,1 +1,23 @@
+import { useState } from 'react';
+import './App.css'
+import ImageAnalysis from './ImageAnalysis/ImageAnalysis';
+import VideoAnalysis from './VideoAnalysis/VideoAnalysis';
+
+function App() {
+  const [framesAvailable, setFramesAvailable] = useState(false)
+  return (
+    <>
+      <div className="title">
+        <p className="product-name">Sponsor Analyzer</p>
+        <p className="team-name">Team G</p>
+      </div>
+
+      <VideoAnalysis />
+      <ImageAnalysis imagesAvailable={framesAvailable} />
+
+    </>
+  );
+}
+
+export default App;
 
